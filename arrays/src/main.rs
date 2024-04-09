f87b85807e1aa2c9f2213d1185d37ea9ed634278
Wojciech Falba <wojciech.falba@gmail.com>
2024-04-09 22:18:40 +0200

    median of 2 sorted arrays / błąd
    median of 2 sorted arrays / błąd
    
    median of 2 soreted arrays / looks like it's working
    
    median of 2 soreted arrays / 1 test fail
    
    median of 2 sorted arrays / works

--- arrays/src/main.rs
+++ arrays/src/main.rs
@@ -1,7 +1,1 @@
-fn main() {
-    let res = ::arrays::median_of_2_sorted_arrays::Solution::find_median_sorted_arrays(
-        vec![1, 2, 5],
-        vec![3, 4, 6],
-    );
-    println!("{:?}", res);
-}
+fn main() {}
--- arrays/src/median_of_2_sorted_arrays.rs
+++ arrays/src/median_of_2_sorted_arrays.rs
@@ -3,38 +3,64 @@ pub struct Solution;
 impl Solution {
     pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
         let l = nums1.len() + nums2.len();
-        Self::next(&nums1, &nums2, 0, &l, &nums1[0])
+        if nums1.is_empty() {
+            return match nums2.len() {
+                1 => nums2[0] as f64,
+                _ => Self::next(&nums2[..l / 2], &nums2[l / 2..], 0, &l, &nums2[0]),
+            };
+        };
+        if nums2.is_empty() {
+            return match nums1.len() {
+                1 => nums1[0] as f64,
+                _ => Self::next(&nums1[..l / 2], &nums1[l / 2..], 0, &l, &nums1[0]),
+            };
+        };
+        Self::next(&nums1, &nums2, 0, &l, &nums1[0].min(nums2[0]))
     }
 
-    fn next(a: &[i32], b: &[i32], i: i32, len: &usize, last: &i32) -> f64 {
-        if i != len.div_ceil(2) as i32 {
-            return if a.len() == 1 {
-                println!("a.len==1, {}, {:?} {:?}", i, a, b);
-                Self::next(b, a, i + 1, len, &a[0])
-            } else if b.len() == 1 {
-                println!("b.len==1, {}, {:?} {:?}", i, a, b);
-                Self::next(&a[1..], b, i + 1, len, &b[0])
-            } else if a[0] < b[0] {
-                println!("a<b, {}, {:?} {:?}", i, a, b);
-                Self::next(&a[1..], b, i + 1, len, &a[0])
+    fn next(left: &[i32], right: &[i32], deepness: i32, length: &usize, previous: &i32) -> f64 {
+        println!(
+            "{:?} {:?} {} {} {}",
+            left, right, deepness, length, previous
+        );
+        let limit = length.div_ceil(2) as i32;
+        if deepness != limit {
+            return if left.is_empty() && right.is_empty() {
+                *previous as f64
+            } else if left.is_empty() {
+                let r = match right.len() {
+                    1 => &[],
+                    _ => &right[1..],
+                };
+                Self::next(r, left, deepness + 1, length, &right[0])
+            } else if right.is_empty() {
+                Self::next(&left[1..], right, deepness + 1, length, &left[0])
+            } else if left[0] < right[0] {
+                let l = match left.len() {
+                    1 => &[],
+                    _ => &left[1..],
+                };
+                Self::next(l, right, deepness + 1, length, &left[0])
             } else {
-                println!("a>b, {}, {:?} {:?}", i, a, b);
-                Self::next(a, &b[1..], i + 1, len, &b[0])
+                let r = match right.len() {
+                    1 => &[],
+                    _ => &right[1..],
+                };
+                Self::next(left, r, deepness + 1, length, &right[0])
             };
         }
-        if len % 2 == 0 {
-            println!("end, len even, {}, {:?} {:?}", i, a, b);
-            return if a[0] < b[0] {
-                (a[0] + *last) as f64 / 2f64
+        if length % 2 == 0 {
+            return if left.is_empty() {
+                (right[0] + *previous) as f64 / 2f64
+            } else if right.is_empty() {
+                (left[0] + *previous) as f64 / 2f64
+            } else if left[0] > right[0] {
+                (right[0] + *previous) as f64 / 2f64
             } else {
-                (b[0] + *last) as f64 / 2f64
+                (left[0] + *previous) as f64 / 2f64
             };
         }
-        println!("end, len odd, {}, {:?} {:?}", i, a, b);
-        if a[0] < b[0] {
-            return a[0] as f64;
-        }
-        b[0] as f64
+        *previous as f64
     }
 }
 
@@ -43,14 +69,68 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_case_1() {
+    fn two_and_one() {
         let result = Solution::find_median_sorted_arrays(vec![1, 3], vec![2]);
         assert_eq!(result, 2.0f64);
     }
 
     #[test]
-    fn test_case_2() {
+    fn two_and_two_1() {
         let result = Solution::find_median_sorted_arrays(vec![1, 2], vec![3, 4]);
+        assert_eq!(result, 2.5f64);
+    }
+
+    #[test]
+    fn two_and_two_2() {
+        let result = Solution::find_median_sorted_arrays(vec![1, 3], vec![2, 7]);
+        assert_eq!(result, 2.5f64);
+    }
+
+    #[test]
+    fn two_and_two_3() {
+        let result = Solution::find_median_sorted_arrays(vec![1, 3], vec![2, 4]);
+        assert_eq!(result, 2.5f64);
+    }
+
+    #[test]
+    fn empty_and_one() {
+        let result = Solution::find_median_sorted_arrays(vec![], vec![3]);
+        assert_eq!(result, 3.0f64);
+    }
+
+    #[test]
+    fn one_and_empty() {
+        let result = Solution::find_median_sorted_arrays(vec![1], vec![]);
+        assert_eq!(result, 1.0f64);
+    }
+
+    #[test]
+    fn three_and_two() {
+        let result = Solution::find_median_sorted_arrays(vec![1, 3, 5], vec![2, 4]);
+        assert_eq!(result, 3.0f64);
+    }
+
+    #[test]
+    fn eight_and_two() {
+        let result = Solution::find_median_sorted_arrays(vec![1, 3, 5, 6, 7, 8, 9, 10], vec![2, 4]);
+        assert_eq!(result, 5.5f64);
+    }
+
+    #[test]
+    fn empty_and_nine() {
+        let result = Solution::find_median_sorted_arrays(vec![], vec![1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        assert_eq!(result, 5.0f64);
+    }
+
+    #[test]
+    fn five_and_four() {
+        let result = Solution::find_median_sorted_arrays(vec![1, 3, 5, 7, 9], vec![2, 4, 6, 8]);
+        assert_eq!(result, 5.0f64);
+    }
+
+    #[test]
+    fn one_and_three() {
+        let result = Solution::find_median_sorted_arrays(vec![1], vec![2, 3, 4]);
         assert_eq!(result, 2.5f64);
     }
 }
